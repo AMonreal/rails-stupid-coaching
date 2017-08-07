56d98c4aa90d07464a682132f8b82e4ddbea8e82
Ariane Monreal <arianemonreal@hotmail.fr>
2017-08-07 14:52:37 +0200

    exo 1

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -4,5 +4,16 @@ class CoachingController < ApplicationController
 
   def answer
     @answer = params[:question]
+     if @answer.include? "?"
+      @coach_answer = "Silly question, get dressed and go to work!"
+    elsif @answer == "I am going to work right now!"
+      @coach_answer = ""
+    elsif @answer == "I AM GOING TO WORK RIGHT NOW!"
+      @coach_answer = ""
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+    return @coach_answer
   end
 end
+
